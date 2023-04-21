c78d82843b61729b0a56cf62535e0aa984c5b863
polymathLTE <lawaltemmanuel@gmail.com>
2023-04-21 12:36:52 +0100

    Added README and task file 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Using Puppet, creates a file in /tmp
+file { '/tmp/school':
+  ensure  =>  file,
+  path    =>  '/tmp/school',
+  content =>  'I love Puppet',
+  mode    =>  '0744',
+  owner   =>  www-data,
+  group   =>  www-data,
+}
